19997871f1dbf8a9dfb10f15362fec790f745b59
Nicholas Reed <39307926+nreedFA@users.noreply.github.com>
2018-09-14 12:14:30 -0400

    Add files via upload

---
+++ src/main/java/com/fusionalliance/App.java
@@ -0,0 +1,85 @@
+package com.fusionalliance;
+
+import org.apache.http.util.Asserts;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class App {
+    
+    public static void main(String[] args) {
+        // Create a new instance of the Chrome driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+        
+        //Close the browser
+        driver.quit();
+    }
+
+    public static void notmain(String[] args) {
+        // Create a new instance of the Chrome driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit Google
+        driver.get("http://fusionalliance.com/contact/");
+
+        // Find the Name field text input element by its name
+        WebElement nameElement = driver.findElement(By.name("your-name"));
+
+        // Enter Name
+        nameElement.sendKeys("Nicholas Reed");        
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        nameElement.submit();        
+
+        //Validate that Submit didn't work - check for presence of tool tip
+        int alertMessageCount = driver.findElements(By.className("wpcf7-not-valid-tip")).size();
+
+        Asserts.check(alertMessageCount > 0, "alertMessage Exists on page");
+        
+        //Find the email test input element by its name
+        WebElement emailElement = driver.findElement(By.name("email"));
+
+        // Enter Email
+        emailElement.sendKeys("nreed@fusionalliance.com");
+
+       // Now submit the form. WebDriver will find the form for us from the element
+       nameElement.submit();
+
+       driver.quit();
+    }
+}
